415b8a2db630bee6adbb53b7df506fb74322556a
Nagib Alexandre <80482671+NagibAlexandre@users.noreply.github.com>
2023-07-02 12:27:23 -0300

    Delete codigo/feedME/notificacao directory

--- codigo/feedME/notificacao/notificação.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('email-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var email = document.getElementById('email-input').value;
-  if (email.trim() !== '') {
-    alert('Obrigado por se inscrever para receber notificações por email!');
-    // Aqui você pode adicionar a lógica para enviar o email ou fazer outras ações necessárias
-  }
-});
